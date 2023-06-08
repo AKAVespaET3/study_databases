fd82b4c2fd58db13a0b69b569713abea171919aa
AKAVespaET3 <quattrini244@gmail.com>
2023-06-08 11:01:23 +0900

    2023-06-08

--- sqls/ProductsWhereGroupby.sql
+++ sqls/ProductsWhereGroupby.sql
@@ -3,3 +3,11 @@ SELECT COUNT(*) AS COUNT_CategoryID , PD.CategoryID
 FROM Products AS PD
 GROUP BY PD.CategoryID
 HAVING COUNT(PD.CategoryID) >= 10;
+
+-- Number of Records: 5
+-- COUNT_CategoryID	CategoryID
+-- 12	1
+-- 12	2
+-- 13	3
+-- 10	4
+-- 12	8
